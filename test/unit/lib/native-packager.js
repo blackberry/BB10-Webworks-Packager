167636e085ad0ff6021bf062829522fc716ac23f
Rosa Tse <rtse@rim.com>
2012-02-09 17:30:32 -0500

    Generate frameworkModules.js; Changed native-packager exec to take callback to notify caller; Fixed native-packager unit test

--- test/unit/lib/native-packager.js
+++ test/unit/lib/native-packager.js
@@ -1,6 +1,5 @@
 var path = require("path"),
     srcPath = __dirname + "/../../../lib/",
-    fs = require("fs"),
     wrench = require("wrench"),
     nativePkgr = require(path.resolve(srcPath + "/native-packager")),
     files = require(path.resolve(srcPath + "/file-manager")),
@@ -11,22 +10,29 @@ describe("Native packager", function () {
         if (path.existsSync("dependencies/tools/bin")) {
             var outputDir = path.resolve("test_bar"),
                 session = {
-                "barPath": path.resolve(outputDir + "/Demo.bar"),
-                "outputDir": outputDir,
-                "sourceDir": path.resolve(outputDir + "/src"),
-                "archivePath": path.resolve("test/test.zip"),
-                "conf": require(path.resolve(srcPath + "/conf")),
-                "targets": ["simulator"]
-            };
+                    "barPath": path.resolve(outputDir + "/Demo.bar"),
+                    "outputDir": outputDir,
+                    "sourceDir": path.resolve(outputDir + "/src"),
+                    "archivePath": path.resolve("test/test.zip"),
+                    "conf": require(path.resolve(srcPath + "/conf")),
+                    "targets": ["simulator"]
+                },
+                config = {
+                    "id": 'Demo',
+                    "name": 'Demo',
+                    "versionNumber": '1.0.0',
+                    "author": 'Research In Motion Ltd.',
+                    "description": 'This is a test!',
+                    "image": 'test.png'
+                };
 
             if (path.existsSync(session.outputDir)) {
                 wrench.rmdirSyncRecursive(session.outputDir);
             }
 
             files.prepare(session, []);
-            nativePkgr.exec(session);
-
-            fs.watch(outputDir, function (event, filename) {
+            nativePkgr.exec(session, config, function (code) {
+                expect(code).toEqual(0);
                 expect(path.existsSync(session.barPath)).toBeTruthy();
             });
         }
