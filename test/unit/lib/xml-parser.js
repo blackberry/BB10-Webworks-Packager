8d66ecb8935dabda092449b49751f622134ba6cf
Rosa Tse <rtse@rim.com>
2012-02-16 15:17:20 -0500

    Updated unit tests for xml-parser

--- test/unit/lib/xml-parser.js
+++ test/unit/lib/xml-parser.js
@@ -4,12 +4,28 @@ var testData = require("./test-data"),
 
 describe("xml parser", function () {
     it("parses a config.xml", function () {
-        xmlParser.parse(testData.configPath, function (configObj) {
+        var configPath = path.resolve("test/config.xml");
+
+        xmlParser.parse(configPath, function (configObj) {
             expect(configObj.content).toEqual("local:///startPage.html");
+            expect(configObj.id).toEqual("MyWidgetId");
             expect(configObj.version).toEqual("1.0.0");
-            expect(configObj.foregroundSource).toEqual("local:///startPage.html");
+            expect(configObj.license).toEqual("My License");
+            expect(configObj.licenseURL).toEqual("http://www.apache.org/licenses/LICENSE-2.0");
             expect(configObj.icon).toEqual("test.png");
             expect(configObj.configXML).toEqual("config.xml");
+            expect(configObj.author).toEqual("Research In Motion Ltd.");
+            expect(configObj.name).toEqual("Demo");
+            expect(configObj.description).toEqual("This app does everything.");
+        });
+    });
+
+    it("parses a bare minimum config.xml without error", function () {
+        var configPath = path.resolve("test/config-bare-minimum.xml");
+
+        xmlParser.parse(configPath, function (configObj) {
+            expect(configObj.content).toEqual("local:///startPage.html");
+            expect(configObj.version).toEqual("1.0.0");
         });
     });
 });
