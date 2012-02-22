17b75d43d4b679cf12e7bdc9a3127ed235b27e16
Rosa Tse <rtse@rim.com>
2012-02-22 14:48:37 -0500

    Added unit test for bar-builder

---
+++ test/unit/lib/zbar-builder.js
@@ -0,0 +1,13 @@
+var srcPath = __dirname + "/../../../lib/",
+    path = require("path"),
+    barBuilder = require(srcPath + "bar-builder"),
+    testData = require("./test-data");
+
+describe("BAR builder", function () {
+    it("build() create BAR for specified session", function () {
+        barBuilder.build(testData.session, testData.config, function() {
+            fileMgr.cleanSource(testData.session);
+            expect(path.existsSync(testData.session.barPath)).toBeTruthy();
+        });
+    });
+});
