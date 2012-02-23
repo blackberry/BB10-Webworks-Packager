041fa022cac4b25aa9cc5eafc5c7fd064139aae2
Rosa Tse <rtse@rim.com>
2012-02-23 15:42:57 -0500

    Mocked all dependencies in native-packager and bar-builder to avoid wait completely

--- test/unit/lib/bar-builder.js
+++ test/unit/lib/bar-builder.js
@@ -1,36 +1,29 @@
 var srcPath = __dirname + "/../../../lib/",
     path = require("path"),
-    util = require("util"),
     wrench = require("wrench"),
     barBuilder = require(srcPath + "bar-builder"),
     fileMgr = require(srcPath + "file-manager"),
+    nativePkgr = require(srcPath + "native-packager"),
     logger = require(srcPath + "logger"),
-    testData = require("./test-data"),
-    session = testData.session;
+    testData = require("./test-data");
 
 describe("BAR builder", function () {
-    beforeEach(function () {
-        if (path.existsSync(session.outputDir)) {
-            wrench.rmdirSyncRecursive(session.outputDir);
-        }
-
-        fileMgr.prepareOutputFiles(session, []);
-    });
-
     it("build() create BAR for specified session", function () {
         var callback = jasmine.createSpy(),
+            session = testData.session,
             target = session.targets[0];
+
+        spyOn(wrench, "mkdirSyncRecursive");
+        spyOn(fileMgr, "copyWWE");
+        spyOn(nativePkgr, "exec").andCallFake(function (session, target, config, callback) {
+            callback(0);
+        });
 
         barBuilder.build(session, testData.config, callback);
 
-        waitsFor(function () {
-            return path.existsSync(util.format(session.barPath, target));
-        });
-
-        waits(2000);
-
-        runs(function () {
-            expect(callback).toHaveBeenCalledWith(0);
-        });
+        expect(wrench.mkdirSyncRecursive).toHaveBeenCalledWith(session.outputDir + "/" + target);
+        expect(fileMgr.copyWWE).toHaveBeenCalledWith(session, target);
+        expect(nativePkgr.exec).toHaveBeenCalled();
+        expect(callback).toHaveBeenCalledWith(0);
     });
 });
--- test/unit/lib/native-packager.js
+++ test/unit/lib/native-packager.js
@@ -1,42 +1,59 @@
 var path = require("path"),
     util = require("util"),
-    srcPath = __dirname + "/../../../lib/",
+    fs = require("fs"),
+    childProcess = require("child_process"),
     wrench = require("wrench"),
-    nativePkgr = require(path.resolve(srcPath + "/native-packager")),
-    fileMgr = require(path.resolve(srcPath + "/file-manager")),
-    testData = require("./test-data"),
-    session = testData.session,
-    target = session.targets[0];
+    srcPath = __dirname + "/../../../lib/",
+    nativePkgr = require(srcPath + "/native-packager"),
+    pkgrUtils = require(srcPath + "/packager-utils"),
+    testData = require("./test-data");
 
 describe("Native packager", function () {
-    beforeEach(function () {
-        if (path.existsSync("dependencies/tools/bin")) {
-            if (path.existsSync(session.outputDir)) {
-                wrench.rmdirSyncRecursive(session.outputDir);
-            }
-
-            fileMgr.prepareOutputFiles(session, []);
-            wrench.mkdirSyncRecursive(session.outputDir + "/" + target);
-            fileMgr.copyWWE(session, target);
-        }
-    });
-
     it("exec blackberry-nativepackager", function () {
         if (path.existsSync("dependencies/tools/bin")) {
-            var callback = jasmine.createSpy();
+            var callback = jasmine.createSpy(),
+                config = testData.config,
+                session = testData.session,
+                target = session.targets[0],
+                result = {
+                    stdout: {
+                        on: jasmine.createSpy()
+                    },
+                    stderr: {
+                        on: jasmine.createSpy()
+                    },
+                    on: function (eventName, callback) {
+                        callback(0);
+                    }
+                },
+                bbTabletXML = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                    "<qnx><id>" + config.id + "</id>" +
+                    "<name>" + config.name + "</name>" +
+                    "<versionNumber>" + config.version + "</versionNumber>" +
+                    "<author>" + config.author + "</author>" +
+                    "<asset entry=\"true\" type=\"qnx/elf\">wwe</asset>" +
+                    "<initialWindow><systemChrome>none</systemChrome><transparent>true</transparent></initialWindow>" +
+                    "<env value=\"12\" var=\"WEBKIT_NUMBER_OF_BACKINGSTORE_TILES\"></env>" +
+                    "<permission system=\"true\">run_native</permission>" +
+                    "<description>" + config.description + "</description></qnx>",
+                cmd = path.normalize(session.conf.DEPENDENCIES_TOOLS + "/bin/blackberry-nativepackager.bat");
+
+            spyOn(wrench, "readdirSyncRecursive").andReturn(["abc", "xyz"]);
+            spyOn(fs, "statSync").andReturn({
+                isDirectory: function () {
+                    return false;
+                }
+            });
+            spyOn(fs, "writeFileSync");
+            spyOn(pkgrUtils, "writeFile");
+            spyOn(childProcess, "spawn").andReturn(result);
 
             nativePkgr.exec(session, target, testData.config, callback);
 
-            waitsFor(function () {
-                return path.existsSync(util.format(session.barPath, target));
-            });
-
-            waits(2000);
-
-            runs(function () {
-                expect(path.existsSync(util.format(session.barPath, target))).toBeTruthy();
-                expect(callback).toHaveBeenCalledWith(0);
-            });
+            expect(fs.writeFileSync).toHaveBeenCalledWith(jasmine.any(String), jasmine.any(String));
+            expect(pkgrUtils.writeFile).toHaveBeenCalledWith(session.sourceDir, "blackberry-tablet.xml", bbTabletXML);
+            expect(childProcess.spawn).toHaveBeenCalledWith(cmd, ["@options"], {"cwd": session.sourceDir, "env": process.env});
+            expect(callback).toHaveBeenCalledWith(0);
         }
     });
 });
