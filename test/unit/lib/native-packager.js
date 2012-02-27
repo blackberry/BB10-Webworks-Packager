139794767a6e759b050d6d84cfa0c38a5dfd9495
Sergey Golod <sgolod@rim.com>
2012-02-27 14:52:07 -0500

    Show warning instead of error when degug token path is not valid and added unit test

--- test/unit/lib/native-packager.js
+++ test/unit/lib/native-packager.js
@@ -6,50 +6,80 @@ var path = require("path"),
     srcPath = __dirname + "/../../../lib/",
     nativePkgr = require(srcPath + "/native-packager"),
     pkgrUtils = require(srcPath + "/packager-utils"),
-    testData = require("./test-data");
+    testData = require("./test-data"),
+    logger = require(srcPath + "logger"),
+    callback,
+    config,
+    session,
+    target,
+    result,
+    orgDebugEnabled,
+    orgDebugTokenPath;
 
 describe("Native packager", function () {
+    beforeEach(function () {
+        callback = jasmine.createSpy(),
+        config = testData.config,
+        session = testData.session,
+        target = session.targets[0],
+        result = {
+            stdout: {
+                on: jasmine.createSpy()
+            },
+            stderr: {
+                on: jasmine.createSpy()
+            },
+            on: function (eventName, callback) {
+                callback(0);
+            }
+        },
+        bbTabletXML = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+            "<qnx><id>" + config.id + "</id>" +
+            "<name>" + config.name + "</name>" +
+            "<versionNumber>" + config.version + "</versionNumber>" +
+            "<author>" + config.author + "</author>" +
+            "<asset entry=\"true\" type=\"qnx/elf\">wwe</asset>" +
+            "<initialWindow><systemChrome>none</systemChrome><transparent>true</transparent></initialWindow>" +
+            "<env value=\"12\" var=\"WEBKIT_NUMBER_OF_BACKINGSTORE_TILES\"></env>" +
+            "<permission system=\"true\">run_native</permission>" +
+            "<description>" + config.description + "</description></qnx>",
+        cmd = path.normalize(session.conf.DEPENDENCIES_TOOLS + "/bin/blackberry-nativepackager" + (pkgrUtils.isWindows() ? ".bat" : ""));
+
+        orgDebugEnabled = session.debug;
+        orgDebugTokenPath = session.conf.DEBUG_TOKEN;
+
+        spyOn(wrench, "readdirSyncRecursive").andReturn(["abc", "xyz"]);
+        spyOn(fs, "statSync").andReturn({
+            isDirectory: function () {
+                return false;
+            }
+        });
+        spyOn(fs, "writeFileSync");
+        spyOn(pkgrUtils, "writeFile");
+        spyOn(childProcess, "spawn").andReturn(result);
+    });
+    
+    afterEach(function () {
+        session.debug = orgDebugEnabled;
+        session.conf.DEBUG_TOKEN = orgDebugTokenPath;
+    });
+
+    it("shows debug token warning when path to file is not valid", function () {
+        spyOn(logger, "warn");
+
+        session.debug = true;
+        //Current time will ensure that the file doesn't exist.
+        session.conf.DEBUG_TOKEN = new Date().getTime() + ".bar";
+
+        nativePkgr.exec(session, target, testData.config, callback);
+
+        expect(logger.warn).toHaveBeenCalledWith(jasmine.any(String));
+    });
+
     it("exec blackberry-nativepackager", function () {
         if (path.existsSync("dependencies/tools/bin")) {
-            var callback = jasmine.createSpy(),
-                config = testData.config,
-                session = testData.session,
-                target = session.targets[0],
-                result = {
-                    stdout: {
-                        on: jasmine.createSpy()
-                    },
-                    stderr: {
-                        on: jasmine.createSpy()
-                    },
-                    on: function (eventName, callback) {
-                        callback(0);
-                    }
-                },
-                bbTabletXML = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                    "<qnx><id>" + config.id + "</id>" +
-                    "<name>" + config.name + "</name>" +
-                    "<versionNumber>" + config.version + "</versionNumber>" +
-                    "<author>" + config.author + "</author>" +
-                    "<asset entry=\"true\" type=\"qnx/elf\">wwe</asset>" +
-                    "<initialWindow><systemChrome>none</systemChrome><transparent>true</transparent></initialWindow>" +
-                    "<env value=\"12\" var=\"WEBKIT_NUMBER_OF_BACKINGSTORE_TILES\"></env>" +
-                    "<permission system=\"true\">run_native</permission>" +
-                    "<description>" + config.description + "</description></qnx>",
-                cmd = path.normalize(session.conf.DEPENDENCIES_TOOLS + "/bin/blackberry-nativepackager" + (pkgrUtils.isWindows() ? ".bat" : ""));
-
-            spyOn(wrench, "readdirSyncRecursive").andReturn(["abc", "xyz"]);
-            spyOn(fs, "statSync").andReturn({
-                isDirectory: function () {
-                    return false;
-                }
-            });
-            spyOn(fs, "writeFileSync");
-            spyOn(pkgrUtils, "writeFile");
-            spyOn(childProcess, "spawn").andReturn(result);
-
             nativePkgr.exec(session, target, testData.config, callback);
-
+            
             expect(fs.writeFileSync).toHaveBeenCalledWith(jasmine.any(String), jasmine.any(String));
             expect(pkgrUtils.writeFile).toHaveBeenCalledWith(session.sourceDir, "blackberry-tablet.xml", bbTabletXML);
             expect(childProcess.spawn).toHaveBeenCalledWith(cmd, ["@options"], {"cwd": session.sourceDir, "env": process.env});
