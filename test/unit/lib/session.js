f33263624bbc19dea9d02281f976c2e9dbbfe73c
James Keshavarzi <jkeshavarzi@rim.com>
2012-04-12 15:53:40 -0400

    Fixes GHI #85 Packager error when output path is relative + unit tests

--- test/unit/lib/session.js
+++ test/unit/lib/session.js
@@ -3,6 +3,14 @@ var session = require(__dirname + "/../../../lib/session"),
     wrench = require("wrench"),
     zipLocation = __dirname + "/../../config.xml";
 
+function mockResolve() {
+    //Mocking resolve because of a weird issue where resolve would return an 
+    //invalid path on Mac if it cannot find the directory (c:/ doesnt exist on mac)
+    spyOn(path, "resolve").andCallFake(function (myPath) {
+        return path.normalize(myPath);
+    });
+}
+    
 describe("Session", function () {
     beforeEach(function () {
         //Do not create the source folder
@@ -10,6 +18,8 @@ describe("Session", function () {
     });
     
     it("sets the source directory correctly when specified [-s C:/sampleApp/mySource]", function () {
+        mockResolve();
+        
         var data = {
             args: [ 'C:/sampleApp/sample.zip' ],
             output: 'C:/sampleApp/bin',
@@ -21,6 +31,8 @@ describe("Session", function () {
     });
     
     it("sets the source directory correctly when unspecified [-s] and output path set [-o]", function () {
+        mockResolve();
+        
         var data = {
             args: [ 'C:/sampleApp/sample.zip' ],
             output: 'C:/sampleApp/bin',
@@ -33,6 +45,8 @@ describe("Session", function () {
     });
     
     it("sets the source directory correctly when unspecified [-s] and no output path is set", function () {
+        mockResolve();
+        
         var data = {
             args: [ zipLocation ],
             source: true//equivalent to [-s]
@@ -65,5 +79,17 @@ describe("Session", function () {
         result = session.initialize(data);
         
         expect(result.buildId).toEqual('100');
+    });
+    
+    it("sets the output directory correctly when specified with a relative path [-o myOutput]", function () {
+        var bbwpDir = __dirname + "/../../../",
+        data = {
+            args: [ 'C:/sampleApp/sample.zip' ],
+            output: 'myOutput',
+        },
+        result = session.initialize(data);
+        
+        //output should be set to bbwp location + outputFolder
+        expect(result.outputDir).toEqual(path.normalize(path.join(bbwpDir, "myOutput")));
     });
 });
