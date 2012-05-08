cf84ed118deab5d5b2bbe02ed2b0498afbe84d23
Henry Aghaulor <haghaulor@rim.com>
2012-05-08 09:56:59 -0400

    Made the following changes: 1. Changed the layout of unit tests for signing helper. 2. Changed unit tests for signing helper to preserve properties that are mocked.

--- test/unit/lib/signing-helper.js
+++ test/unit/lib/signing-helper.js
@@ -2,91 +2,140 @@ var testData = require('./test-data'),
     signingHelper = require(testData.libPath + '/signing-helper'),
     localize = require(testData.libPath + '/localize'),
     path = require('path'),
-    os = require('os');
+    os = require('os'),
+    properties = { 
+        homepath: "",
+        homedrive: ""
+    };
 
 describe("signing-helper", function () {
-    it("can find keys in windows Local Settings", function () {
-        spyOn(os, "type").andReturn("windows");
+
+    describe("windows path to signing keys", function () {
         
-        spyOn(path, "existsSync").andCallFake(function (path) {
-            return path.indexOf("\\Local Settings") !== -1;
+        beforeEach(function () {
+            
+            if (typeof process.env.HOMEPATH === 'undefined') {
+                properties.homepath = "delete";
+            } else {
+                properties.homepath = process.env.HOMEPATH;
+            }
+
+            if (typeof process.env.HOMEDRIVE === 'undefined') {
+                properties.homedrive = "delete";
+            } else {
+                properties.homedrive = process.env.HOMEDRIVE;
+            }
         });
         
-        var result = signingHelper.getKeyStorePath();
-        expect(result).toContain("\\Local Settings");
-    });
-    
-    it("can find keys in windows AppData", function () {
-        spyOn(os, "type").andReturn("windows");
-        
-        spyOn(path, "existsSync").andCallFake(function (path) {
-            return path.indexOf("\\AppData") !== -1;
-        });
-        
-        var result = signingHelper.getKeyStorePath();
-        expect(result).toContain("\\AppData");
-    });
-    
-    it("returns a path on windows that specifies the drive", function () {
-        process.env.HOMEPATH = "Users\\user";
-        process.env.HOMEDRIVE = "C:";
-        
-        this.after = function () {
-            delete process.env.HOMEPATH;
-            delete process.env.HOMEDRIVE;
-        };
+        afterEach(function () {
 
-        spyOn(os, "type").andReturn("windows");
-        spyOn(path, "existsSync").andCallFake(function (p) {
-            return p.indexOf("C:") !== -1;
+            if (typeof process.env.HOMEPATH === 'string') {
+                if (properties.homepath === 'delete') {
+                    delete process.env.HOMEPATH;
+                } else {
+                    process.env.HOMEPATH = properties.homepath; 
+                }
+            }
+
+            if (typeof process.env.HOMEDRIVE === 'string') {
+                if (properties.homedrive === 'delete') {
+                    delete process.env.HOMEDRIVE;
+                } else {
+                    process.env.HOMEDRIVE = properties.homedrive; 
+                }
+            }
         });
 
-        var result = signingHelper.getKeyStorePath();
-        expect(result).toContain("C:");
-    });
-
-    it("can find keys in windows on a different path", function () {
-        process.env.HOMEPATH = "Users\\User";
-        process.env.HOMEDRIVE = "D:";
-
-        this.after = function () {
-            delete process.env.HOMEPATH;
-            delete process.env.HOMEDRIVE;
-        };
-
-        spyOn(os, "type").andReturn("windows");
-        spyOn(path, "existsSync").andCallFake(function (path) {
-            return path.indexOf("D:") !== -1;
-        });
-
-        var result = signingHelper.getKeyStorePath();
-        expect(result).toContain("D:");
-    });
-    
-    it("can find keys in the Mac Library folder", function () {
-        spyOn(os, "type").andReturn("darwin");
-        
-        spyOn(path, "existsSync").andCallFake(function (path) {
-            return path.indexOf("/Library/Research In Motion/") !== -1;
+        it("contains Local Settings", function () {
+            spyOn(os, "type").andReturn("windows");
+            
+            spyOn(path, "existsSync").andCallFake(function (path) {
+                return path.indexOf("\\Local Settings") !== -1;
+            });
+            
+            var result = signingHelper.getKeyStorePath();
+            expect(result).toContain("\\Local Settings");
         });
         
-        var result = signingHelper.getKeyStorePath();
-        expect(result).toContain("/Library/Research In Motion/");
-    });
+        it("contains AppData", function () {
+            spyOn(os, "type").andReturn("windows");
+            
+            spyOn(path, "existsSync").andCallFake(function (path) {
+                return path.indexOf("\\AppData") !== -1;
+            });
+            
+            var result = signingHelper.getKeyStorePath();
+            expect(result).toContain("\\AppData");
+        });
     
-    it("returns undefined on windows when keys cannot be found", function () {
-        spyOn(os, "type").andReturn("windows");
-        spyOn(path, "existsSync").andReturn(false);
-        
-        var result = signingHelper.getKeyStorePath();
-        expect(result).toBeUndefined();
+        it("contains the home path", function () {
+            process.env.HOMEPATH = "Users\\user";
+            process.env.HOMEDRIVE = "C:";
+
+            spyOn(os, "type").andReturn("windows");
+            spyOn(path, "existsSync").andCallFake(function (p) {
+                return p.indexOf("C:") !== -1;
+            });
+
+            var result = signingHelper.getKeyStorePath();
+            expect(result).toContain("C:");
+        });
+
+        it("specifies the drive", function () {
+
+            process.env.HOMEPATH = "Users\\user";
+            process.env.HOMEDRIVE = "C:";
+            
+            spyOn(os, "type").andReturn("windows");
+            spyOn(path, "existsSync").andCallFake(function (p) {
+                return p.indexOf("C:") !== -1;
+            });
+
+            var result = signingHelper.getKeyStorePath();
+            expect(result).toContain("C:");
+        });
+
+        it("specifies a drive other than C", function () {
+            process.env.HOMEPATH = "Users\\User";
+            process.env.HOMEDRIVE = "D:";
+
+            spyOn(os, "type").andReturn("windows");
+            spyOn(path, "existsSync").andCallFake(function (path) {
+                return path.indexOf("D:") !== -1;
+            });
+
+            var result = signingHelper.getKeyStorePath();
+            expect(result).toContain("D:");
+        });
+
+        it("is undefined when keys cannot be found", function () {
+            spyOn(os, "type").andReturn("windows");
+            spyOn(path, "existsSync").andReturn(false);
+            
+            var result = signingHelper.getKeyStorePath();
+            expect(result).toBeUndefined();
+        });
     });
-    
-    it("returns undefined on mac when keys cannot be found", function () {
-        spyOn(os, "type").andReturn("darwin");
-        spyOn(path, "existsSync").andReturn(false);
+
+    describe("mac path to signing keys", function () {
+
+        it("contains the Library folder", function () {
+            spyOn(os, "type").andReturn("darwin");
+            
+            spyOn(path, "existsSync").andCallFake(function (path) {
+                return path.indexOf("/Library/Research In Motion/") !== -1;
+            });
+            
+            var result = signingHelper.getKeyStorePath();
+            expect(result).toContain("/Library/Research In Motion/");
+        });
         
-        var result = signingHelper.getKeyStorePath();
-        expect(result).toBeUndefined();
+        it("is undefined when keys cannot be found", function () {
+            spyOn(os, "type").andReturn("darwin");
+            spyOn(path, "existsSync").andReturn(false);
+            
+            var result = signingHelper.getKeyStorePath();
+            expect(result).toBeUndefined();
+        });
     });
 });
